9394ba45216037014d0914fec489a9cf984095bb
asherlie <lieber.asher@gmail.com>
2021-11-07 09:12:55 -0500

    fixed potential timing issue - pop_mq() now doesn't block unless queue is empty

--- mq.c
+++ mq.c
@@ -33,6 +33,10 @@ void insert_mq(struct mqueue* mq, uint8_t* packet, int len){
     pthread_mutex_unlock(&mq->lock);
 }
 
+_Bool mq_is_empty(struct mqueue* mq){
+    return mq->first;
+}
+
 struct mq_entry* pop_mq(struct mqueue* mq){
     struct mq_entry* ret = NULL;
     pthread_mutex_t tmp_lck;
@@ -40,7 +44,11 @@ struct mq_entry* pop_mq(struct mqueue* mq){
 
     while(!ret){
         pthread_mutex_lock(&tmp_lck);
-        pthread_cond_wait(&mq->cond, &tmp_lck);
+        /* mq is non empty no need to sleep
+         * even if another thread gets to mq->head first,
+         * we'll just iterate
+         */
+        if(!mq_is_empty(mq))pthread_cond_wait(&mq->cond, &tmp_lck);
         pthread_mutex_unlock(&tmp_lck);
         pthread_mutex_lock(&mq->lock);
         ret = mq->first;
